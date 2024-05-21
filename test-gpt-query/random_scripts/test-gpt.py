7f7a6c4e57fd4d71b1015471d883d0b0b7588961
Raymond Liu <raymliu@hal9000.ais.sandbox>
2024-05-21 11:14:38 -0400

    Cleaned up more things

--- test-gpt-query/random_scripts/test-gpt.py
+++ test-gpt-query/random_scripts/test-gpt.py
@@ -1,21 +1,29 @@
+"""
+Test the GPT3.5 chat completions API.
+"""
+
 from openai import OpenAI
 import json
 
 def pretty_print(obj):
+    """
+    Print the attributes of obj, separated by \n
+    """
     for key in obj.__dict__:
         print(f"{key}: {obj.__dict__[key]}")
 
-# Sets up connection to the API, need to put OPEN_API_KEY as a variable in the terminal before running
-client = OpenAI()
+if __name__ == "__main__":
+    # Set up connection to the API, need to put OPEN_API_KEY as a variable in the terminal before running
+    client = OpenAI()
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": "Revise and justify the following sentence: Bill is a congressperson."},
-    ],
-    max_tokens=None,
-    n=2,
-)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "Revise and justify the following sentence: Bill is a congressperson."},
+        ],
+        max_tokens=None,
+        n=2,
+    )
 
-#print(json.dumps(json.loads(response.model_dump_json()), indent=4))
-pretty_print(response)
+    # Print the attributes of response
+    pretty_print(response)
