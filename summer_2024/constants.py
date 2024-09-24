6cd132edd4d5ec511bceb4112479f4afb6894f7b
juliawatson <jreneewatson@gmail.com>
2024-09-24 11:43:21 -0400

    Update constants.py
    Added "llama-3.1-8B-Instruct" as a model.

--- summer_2024/constants.py
+++ summer_2024/constants.py
@@ -10,18 +10,19 @@ REQUESTS_PER_MINUTE_LIMIT = 10000
 
 #MODEL_NAME = "gpt-3.5-turbo"
 #MODEL_NAME = "gpt-4-turbo"
-# MODEL_NAME = "gpt-4o"
-MODEL_NAME = "llama-3.1-8B-Instruct"
+MODEL_NAME = "gpt-4o"
+# MODEL_NAME = "llama-3.1-8B-Instruct"
 
 # MODEL_NAMES = ["gpt-3.5-turbo", 'gpt-4-turbo', 'gpt-4o']
 # MODEL_NAMES = ['gpt-3.5-turbo', 'gpt-4o']
-# MODEL_NAMES = ["llama-3.1-8B-Instruct"]
+MODEL_NAMES = ["gpt-4o", "llama-3.1-8B-Instruct"]
 
 AN_NOUNS = {"anchor", "anchorman", "anchorwoman"}
 
-EXPERIMENT_PATH = "piloting_experiment_3_way"
+#EXPERIMENT_PATH = "piloting_experiment_3_way"
 #EXPERIMENT_PATH = "piloting_if_needed"
 #EXPERIMENT_PATH = "piloting_man"
 #EXPERIMENT_PATH = "piloting_non_binary"
 #EXPERIMENT_PATH = "piloting_woman"
 #EXPERIMENT_PATH = "piloting_experiment_2_way"
+EXPERIMENT_PATH = "piloting_pronouns_genders"
