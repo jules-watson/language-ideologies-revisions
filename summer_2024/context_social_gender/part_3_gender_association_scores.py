31431d7af9908ffd04544e0ce303f08bb6522ec9
Xi (Joy) Wang <xxwang007@icloud.com>
2025-02-18 11:18:43 -0500

    updated part_3_gender_association_scores.py

--- summer_2024/context_social_gender/part_3_gender_association_scores.py
+++ summer_2024/context_social_gender/part_3_gender_association_scores.py
@@ -14,6 +14,7 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
+import numpy as np
 from common import load_csv
 from constants import EXPERIMENT_PATH, MODEL_NAME
 
@@ -24,14 +25,18 @@ def main(config_path):
     logprobs_path = f"{dirname}/{MODEL_NAME}/logprobs.csv"
     logprobs_data = load_csv(logprobs_path)
     output_path=f"{dirname}/{MODEL_NAME}"
+
+    prob_woman = np.exp(logprobs_data["woman"])
+    prob_man = np.exp(logprobs_data["man"])
+    prob_person = np.exp(logprobs_data["person"])
     
     print(f"Calculating feminine score for: {MODEL_NAME}")
     # feminine_score = woman / (woman + man)
-    logprobs_data["feminine_score"] = logprobs_data["woman"] / (logprobs_data["woman"] + logprobs_data["man"])
+    logprobs_data["feminine_score"] = prob_woman / (prob_woman + prob_man)
 
     print(f"Calculating genderedness score for: {MODEL_NAME}") 
     # genderedness_score = (woman + man) / (woman + man + person)
-    logprobs_data["genderedness_score"] = (logprobs_data["woman"] + logprobs_data["man"]) / (logprobs_data["woman"] + logprobs_data["man"] + logprobs_data["person"])
+    logprobs_data["genderedness_score"] = (prob_woman + prob_man) / (prob_woman + prob_man + prob_person)
                             
     logprobs_data.to_csv(f'{output_path}/sentence_gender_association_scores.csv', index=False)
 
