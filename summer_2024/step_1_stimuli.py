11f77ba6df03e4c4ace16ad62377f49776c4ca09
Julia Watson <jreneewatson@gmail.com>
2025-03-11 15:15:40 -0400

    Updated code to run full analysis

--- summer_2024/step_1_stimuli.py
+++ summer_2024/step_1_stimuli.py
@@ -11,8 +11,8 @@ import ast
 import collections
 import pandas as pd
 
-from common import load_json, load_csv, gender_list_dict
-from constants import AN_NOUNS, EXPERIMENT_PATH
+from common import load_json, gender_list_dict
+from constants import EXPERIMENT_PATH
 
 
 def add_prompts(data, 
@@ -53,12 +53,13 @@ def main_role_nouns(config, output_dir):
     # Iterate through the role nouns
     for role_noun_set in role_nouns:
         for i in range(len(role_noun_set)):  
+
             # choose a specific role noun variant within a role noun set
             role_noun = role_noun_set[i] 
             matching_rows = about_me_data[about_me_data['filtered_roles_data'].str.contains(f"'{role_noun}'", case=False, na=False)]
             selected_rows = matching_rows.sample(min(10, len(matching_rows)))
 
-            for index, row in selected_rows.iterrows():
+            for _, row in selected_rows.iterrows():
                 sentence = row['sentence']
 
                 # Obtain the corresponding information (i.e. starting and ending index in sentence) of the role noun
@@ -102,6 +103,56 @@ def main_jan1(config, output_dir):
     df.to_csv(output_path)
 
 
+def main_mar7_full_analysis(config, output_dir):
+    
+    # load role noun sets
+    role_nouns_df = pd.read_csv(config["role_nouns"])
+    role_nouns_df["index"] = role_nouns_df["neutral"]
+    role_nouns_df = role_nouns_df.set_index("index")
+
+    # load sentence data
+    about_me_data = pd.read_csv(config["sentence_data"], index_col=0)
+
+    data = collections.defaultdict(list)
+    role_noun_genders = ["neutral", "feminine", "masculine"]
+
+    for _, row in about_me_data.iterrows():
+        sentence = row['sentence_processed']
+        role_noun_original = row["role_noun"]
+        role_noun_original_gender = row["gender"]
+        role_noun_neutral = row["role_noun_set"]
+        role_noun_set = [
+            role_nouns_df.loc[role_noun_neutral][curr_gender]
+            for curr_gender in role_noun_genders
+        ]
+
+        # iterate through substitutions
+        for role_noun_gender in role_noun_genders: 
+            curr_role_noun = role_nouns_df.loc[role_noun_neutral][role_noun_gender]
+
+            # make sentence template
+            assert sentence.count(role_noun_original) == 1
+            sentence_format = sentence.replace(role_noun_original, '[ROLE NOUN]')
+            new_sentence = sentence_format.replace('[ROLE NOUN]', curr_role_noun)
+
+            # This adds prompts for each task wording
+            add_prompts(
+                data=data,
+                role_noun=curr_role_noun,
+                role_noun_gender=role_noun_gender,
+                role_noun_original_gender=role_noun_original_gender,
+                role_noun_is_substituted=(role_noun_gender!=role_noun_original_gender),
+                role_noun_set=role_noun_set,
+                sentence=new_sentence,
+                sentence_format=sentence_format,
+                task_wording=config["task_wording"])
+
+    df = pd.DataFrame(data)
+    df.index.name = "index"
+    output_path = f"{output_dir}/stimuli.csv"
+    df.to_csv(output_path)
+
+
 def main(config_path):
     # Load the config
     config = load_json(config_path)
@@ -110,6 +161,9 @@ def main(config_path):
     # Run the correct main function for the domain
     if config["domain"] == "role_nouns":
         if ("stimuli_method" in config and 
+            config["stimuli_method"] == "mar7_full_analysis"):
+            main_mar7_full_analysis(config, config_dir)
+        elif ("stimuli_method" in config and 
             config["stimuli_method"] == "jan1_piloting_pronouns_genders"):
             main_jan1(config, config_dir)
         else:
